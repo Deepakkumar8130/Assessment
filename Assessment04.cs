f60e2a06aea9d04569f7c45f802c9365f9a9466c
Deepakkumar8130 <139843046+Deepakkumar8130@users.noreply.github.com>
2023-07-21 13:09:38 +0530

    Add files via upload

---
+++ Assessment04.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Text;
+
+namespace Assessment04
+{
+    class Assessment04
+    {
+        static void Q1()
+        {
+            Console.Write("Enter a String :- ");
+            string s = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            for(int i=0; i<s.Length; i++)
+            {
+                if (s[i] >= 'a' && s[i] <= 'z') sb.Append((char)(s[i]-32));
+                else sb.Append(s[i]);
+            }
+
+            Console.Write($"String in Upper case :- {sb.ToString()}");
+            Console.Write(s.ToUpper());
+        }
+
+        //------------------------------------------------------------------------------------------------------------------//
+
+        static void Q2()
+        {
+            Console.Write("Enter The Name :- ");
+            string name = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            string[] name_arr = name.Split(" ");
+
+            for(int i=0; i< name_arr.Length; i++)
+            {
+                if(i == name_arr.Length-1) sb.Append(" "+name_arr[i]);
+                else sb.Append(name_arr[i][0]+".");
+            }
+            Console.Write(sb.ToString());
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------//
+
+        static void Q3()
+        {
+            Console.Write("Enter The Mail Address :- ");
+            string Email_org = Console.ReadLine();
+
+            int Email_length = Email_org.Length;
+            StringBuilder Email_mod = new StringBuilder();
+            Email_mod.Append(Email_org.Substring(0,3));
+
+            for(int  j=3; j<Email_length-10; j++)
+            {
+                Email_mod.Append("*");
+            }
+            Email_mod.Append(Email_org.Substring(Email_length-10));
+
+            Console.Write(Email_mod.ToString());
+        }
+
+        //------------------------------------------------------------------------------------------------------------------//
+
+        static bool search_word(string[] words, string word, int idx)
+        {
+            for(int i=idx+1; i<words.Length; i++)
+            {
+                if (words[i] == word) return true;
+            }
+            return false;
+        }
+        static void Q4()
+        {
+            Console.WriteLine("Enter a string :- ");
+            string S = Console.ReadLine();
+
+            string[] words = S.Split(" ");
+            string[] duplicates_words = new string[S.Length/2];
+            int temp = 0;
+            
+            for(int i=0;i< words.Length;i++)
+            {
+                if (search_word(words, words[i], i))
+                {
+                    if (temp == 0) duplicates_words[temp++] = words[i];
+                    else if (!search_word(duplicates_words, words[i], -1)) duplicates_words[temp++] = words[i];
+                }
+            }
+
+            for(int i=0; i<temp; i++)
+            {
+                Console.WriteLine(duplicates_words[i]);
+            }
+        }
+
+        //------------------------------------------------------------------------------------------------------------------//
+
+        static int search_word2(string[] words, string word, int start, int end)
+        {
+            int count = 0;
+            for(int i=start; i<end; i++)
+            {
+                if (words[i] == word) count++;
+            }
+            return count;
+        }
+        static void Q5()
+        {
+            Console.WriteLine("Enter a string :- ");
+            string S = Console.ReadLine();
+
+            string[] words = S.Split(" ");
+            string[] unique_words = new string[S.Length];
+            int temp = 0;
+
+            unique_words[temp++] = words[0];
+            
+            for(int i=1;i< words.Length;i++)
+            {
+                if (search_word2(unique_words, words[i],0,temp) == 0) unique_words[temp++] = words[i];
+            }
+
+            int[] occurance_word = new int[temp];
+            for(int i=0; i<temp; i++)
+            {
+                occurance_word[i] = search_word2(words, unique_words[i],0,words.Length);
+            }
+            
+            for(int i=0; i<temp; i++)
+            {
+                Console.WriteLine($"Word : {unique_words[i]}, Occurrance : {occurance_word[i]}");
+            }
+        }
+
+        //-------------------------------------------------------------------------------------------------------------------//
+
+        static int search_word3(string chars, char ch , int start, int end)
+        {
+            int count = 0;
+            for (int i = start; i < end; i++)
+            {
+                if (chars[i] == ch) count++;
+            }
+            return count;
+        }
+        static void Q6()
+        {
+            Console.WriteLine("Enter a string :- ");
+            string S = Console.ReadLine();
+
+            StringBuilder unique_char = new StringBuilder();
+            unique_char.Append(S[0]);
+
+            for (int i = 1; i < S.Length; i++)
+            {
+                if (search_word3(unique_char.ToString(), S[i], 0, unique_char.Length) == 0) unique_char.Append(S[i]);
+            }
+
+            int[] occurance_char = new int[unique_char.Length];
+            for (int i = 0; i < unique_char.Length; i++)
+            {
+                occurance_char[i] = search_word3(S, unique_char[i], 0, S.Length);
+            }
+
+            for (int i = 0; i < unique_char.Length; i++)
+            {
+                Console.WriteLine($"Character : {unique_char[i]}, Occurrance : {occurance_char[i]}");
+            }
+        }
+
+
+        //-------------------------------------------------------------------------------------------------------------------//
+
+        static void Q7()
+        {
+            Console.WriteLine("Enter a string :- ");
+            string S = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            for(int i=S.Length-1; i>=0; i--)
+            {
+                sb.Append(S[i]);
+            }
+            if(S.Equals(sb.ToString())) Console.WriteLine($"This is a Palindrome string : {sb.ToString()}");
+            else Console.WriteLine($"This is not a Palindrome string : {sb.ToString()}");
+        }
+        
+        
+        //------------------------------------------------------------------------------------------------------------------//
+
+        static void Q8()
+        {
+            Console.WriteLine("Enter a string :- ");
+            string S = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+
+            for(int i=0;i< S.Length; i++)
+            {
+                if (S[i] >='a' && S[i] <= 'z' || S[i] >= 'A' && S[i] <= 'Z') sb.Append(S[i]);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+        public static void Main(string[] args)
+        {
+           // Q1();
+           // Q2();
+           // Q3();
+           // Q4();
+           // Q5();
+           // Q6();
+           // Q7();
+           // Q8();
+        }
+    }
+}
---
+++ functions.cs
@@ -0,0 +1,141 @@
+﻿namespace Functions
+{
+    class functions
+    {
+        static int multiplication(int a, int b, out bool ch)    // Call by value with out keyword.
+        {
+            if (a * b >= 100) ch = true;
+            else ch = false;
+
+            return a * b;
+        }
+
+
+        static void changes(ref int a, ref int b)   // Call by reference.
+        {
+            a = a + 6;
+            b = b * 4;
+        }
+
+        static int FindMax(params int[] arr)        // Params
+        {
+            int max = 0;
+            foreach (int i in arr)
+            {
+                if (max < i) max = i;
+            }
+            return max;
+        }
+
+        static int multiple(int a , int b = 5)      // For assign default value.
+        {
+            return a * b;
+        }
+
+        public static void Main(String[] args)
+        {
+
+              #region MATH
+
+              decimal n = Convert.ToDecimal(Console.ReadLine());
+
+              Console.WriteLine(Math.Abs(n));       // For getting Absolute Value like if n is -ve then value considered +ve.
+              Console.WriteLine(Math.Ceiling(n));   // For getting choose highest Integer value like if n= 4.4 then they choose 5.
+              Console.WriteLine(Math.Round(n));     // For getting choose highest near Integer value like if n= 4.4 then they choose 4.
+              Console.WriteLine(Math.Floor(n));     // For getting choose Lower Integer value like if n= 4.7 then they choose 4.
+
+              int a = 5, b = 7;
+              Console.WriteLine(Math.Max(a,b));     // For getting a Maximun value of between two numbers.
+              Console.WriteLine(Math.Min(a,b));     // For getting a Minimum value of between two numbers.
+              Console.WriteLine(Math.Pow(a, b));    // For solve power expression like  2^5 = 32. So we write Math.Pow(2, 5).
+              Console.WriteLine(Math.Sqrt(a));      // For solve Square root expression like  √100 = 10. So we write Math.Sqrt(100).
+              #endregion
+
+              #region RANDOM_&_GUID
+              // RANDOM
+              Random number = new Random();
+              var otp= number.Next();               // for getting a random value.
+              Console.WriteLine(otp);
+
+              int otp1= number.Next(1000,9999);     // for getting a random value with bound conditions like number between only 1000 to 9999.
+              Console.WriteLine(otp1);
+
+
+              // GUID
+              //This keyword used for getting a unique ID like - 5caaab6b-171d-4676-b02d-f440a3923898.
+              Guid guid = Guid.NewGuid();
+              Console.WriteLine(guid);
+              #endregion
+
+              #region DATETIME
+
+              var today = DateTime.Now;           // For getting exact now time.
+              Console.WriteLine(today);
+
+
+              Console.WriteLine(today.Date);           // For getting date.
+
+
+              Console.WriteLine(today.Day);           // For getting day 19 limit(1 to 29/30/31)
+              Console.WriteLine(today.DayOfWeek);     // For getting day of week like - Monday.                                          
+              Console.WriteLine(today.DayOfYear);     // For getting day of year like - 125 limit(1 to 366/365).
+
+
+              Console.WriteLine(today.ToShortDateString());   // For get Only Date like 12-10-23.
+              Console.WriteLine(today.ToShortTimeString());   // For get only Time like 12:48 PM.
+
+              Console.WriteLine(today.ToLongDateString());    // For get Date like - Wednesday, July 19, 2023.
+              Console.WriteLine(today.ToLongTimeString());    // For get Time like - 12:48:46 PM.
+              #endregion
+
+              #region CHARACTER HANDLING
+
+              char ch = '5';
+
+              Console.WriteLine(ch);
+
+              Console.WriteLine(char.IsLower(ch));
+
+              Console.WriteLine(char.IsUpper(ch));
+
+              Console.WriteLine(char.IsLetter(ch));
+
+              Console.WriteLine(char.IsLetterOrDigit(ch));
+
+              Console.WriteLine(char.IsPunctuation(ch));
+
+              Console.WriteLine(char.IsNumber(ch));
+
+              Console.WriteLine(char.IsSeparator(ch));
+
+              Console.WriteLine(char.IsWhiteSpace(ch));
+
+              Console.WriteLine(char.IsDigit(ch));
+
+              Console.WriteLine(char.IsControl(ch));
+
+              #endregion
+
+            #region USER DEFINE FUNCTION
+
+            int n1 = 10, n2 = 12;
+            bool check;
+            Console.WriteLine(multiplication(n1, n2, out check)); // if we want to return more than one return statement from methods then we out keyword.
+            Console.WriteLine(check);
+
+            int n3 = 5, n4 = 3;
+            Console.WriteLine($"{n3} and {n4}");
+            changes(ref n3, ref n4);                              // Call by reference.
+            Console.WriteLine($"{n3} and {n4}");
+
+            Console.WriteLine(FindMax(1,2,8,6,10,15));
+            Console.WriteLine(FindMax(1,2,10,15));
+            Console.WriteLine(FindMax(1,10,15));
+            Console.WriteLine(FindMax(10,15));
+
+            Console.WriteLine(multiple(5, 10));
+            Console.WriteLine(multiple(5));
+            #endregion
+        }
+    }
+}
